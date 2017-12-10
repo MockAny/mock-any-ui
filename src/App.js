a28a2d5f178116fbc7b3f8f2136cb889848af55e
Pedro Furst <pedrofurst@gmail.com>
2017-12-10 14:54:04 -0200

    first version

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => (
+  <div className="App">
+    <div className="containeir">
+      <div className="row">
+        <div className="text-center">
+          <h1>Mock Any</h1>
+          <p>You can mock anything.</p>
+          <p>Enjoy!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
